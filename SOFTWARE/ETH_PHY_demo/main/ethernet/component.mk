eef5941146dcd20565e0f6bf85b65b2db7d0c9a8
DanKoloff <lubchoh@yahoo.com>
2018-07-04 13:48:41 +0300

    Fix
    Changed ethernet example

--- SOFTWARE/ETH_PHY_demo/main/ethernet/component.mk
+++ SOFTWARE/ETH_PHY_demo/main/ethernet/component.mk
@@ -6,6 +6,3 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
-
-# COMPONENT_SRCDIRS := . can ir uart i2c sd spi ethernet
-COMPONENT_SRCDIRS := . ethernet sd ir can gpio wifi
